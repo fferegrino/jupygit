8ca1ac5f70ed1d85a77ced5d9988c57110d506bd
Antonio Feregrino <aferegrino@mintel.com>
2018-09-26 07:07:03 +0100

    Reordered some code.

---
+++ jupygit/nb_manipulator.py
@@ -0,0 +1,18 @@
+def clean_nb(self, dirty, outputs_to_remove=[]):
+    cells = dirty.get('cells', [])
+    to_remove = set(outputs_to_remove)
+    for cell in cells:
+        if cell["cell_type"] != "code": continue
+        cell["execution_count"] = None
+        if to_remove:  # WIP
+            ix_to_remove = []
+            for output in cell["outputs"]:
+                if 'data' in output:
+                    keys = output['data'].keys()
+                    print(keys)
+                elif 'name' in output:
+                    print(output)
+                else:
+                    print(output)
+        else:
+            cell["outputs"] = []
