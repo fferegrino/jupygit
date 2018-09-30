46cac3df5cea5eff54f3c979dbc08dc3883bc591
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-09-30 17:31:53 +0100

    Changed parameters, outputs_to_remove is still a proposal.

--- jupygit/nb_manipulator.py
+++ jupygit/nb_manipulator.py
@@ -1,18 +1,6 @@
-def clean_nb(dirty, outputs_to_remove=[]):
+def clean_nb(dirty, preserve=True):
     cells = dirty.get('cells', [])
-    to_remove = set(outputs_to_remove)
     for cell in cells:
         if cell["cell_type"] != "code": continue
         cell["execution_count"] = None
-        if to_remove:  # WIP
-            ix_to_remove = []
-            for output in cell["outputs"]:
-                if 'data' in output:
-                    keys = output['data'].keys()
-                    print(keys)
-                elif 'name' in output:
-                    print(output)
-                else:
-                    print(output)
-        else:
-            cell["outputs"] = []
+        cell["outputs"] = []
